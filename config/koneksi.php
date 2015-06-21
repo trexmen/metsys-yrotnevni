93973e37db6190c2fd07c4cef9b5e257d2f0c5e1
Tarkiman <tarkiman.zone@gmail.com>
2015-06-21 22:42:16 +0700

    update koneksi.php

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,14 +1,14 @@
 <?php
 // definisikan koneksi ke database
-// $server = "localhost";
-// $username = "root";
-// $password = "";
-// $database = "project";
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "inventory_system";
 
-$server = "sql102.my-php.net";
-$username = "my_8456742";
-$password = "warsimdasem";
-$database = "my_8456742_latihan";
+// $server = "sql102.my-php.net";
+// $username = "my_8456742";
+// $password = "warsimdasem";
+// $database = "my_8456742_latihan";
 
 // Koneksi dan memilih database di server
 mysql_connect($server,$username,$password) or die("Koneksi gagal");
